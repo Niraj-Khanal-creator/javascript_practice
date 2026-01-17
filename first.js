05739efc54814a72fdff65e1159006978319547e
Niraj-Khanal-creator <nirajkhanal13@gmail.com>
2026-01-17 17:32:54 +0545

    new file and creation of object

--- first.js
+++ first.js
@@ -2,15 +2,20 @@
 console.log("hello");
 console.log("k xa yrr");
 //variables
-fullname="tony stark";//string
-age=24;//integer
+//string
+fullname="tony stark";
+//boolean
+age=24;x
 console.log(fullname);
 console.log(age);
-x=null; //null value
+ //null value
+x=null;
 console.log(x);
-y=undefined; //undefined
+//undefined
+y=undefined; 
 console.log(y);
-isFollow=false; //boolean
+//boolean
+isFollow=false; 
 console.log(isFollow);
 
 
@@ -19,14 +24,20 @@ console.log(isFollow);
 var as=10; //old version used before 2015 before es6 was introduced
 let a="tony stark"; //new version after es6 was introduced used in present time
 a=10;
-const PI=3.14; //constant
+// const PI=3.14; //constant
 console.log(a);
-console.log(PI);
+// console.log(PI);
 console.log(as);
 
 
 //data types
 let b=10;
 let fullName="tony stark";
-
-
+//we can create an object  by using const
+const student = { 
+    sfullName:"tony stark",
+    sage:89,
+    cgpa:3,
+    isPass:true, 
+};
+console.log(student);
