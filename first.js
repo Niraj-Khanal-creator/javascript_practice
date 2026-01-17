c368ce31b8d91e3fcbf8d6bcde5783577ee4c58a
Niraj-Khanal-creator <nirajkhanal13@gmail.com>
2026-01-17 17:10:03 +0545

    practice one

---
+++ first.js
@@ -0,0 +1,32 @@
+//first 
+console.log("hello");
+console.log("k xa yrr");
+//variables
+fullname="tony stark";//string
+age=24;//integer
+console.log(fullname);
+console.log(age);
+x=null; //null value
+console.log(x);
+y=undefined; //undefined
+console.log(y);
+isFollow=false; //boolean
+console.log(isFollow);
+
+
+
+//let const var
+var as=10; //old version used before 2015 before es6 was introduced
+let a="tony stark"; //new version after es6 was introduced used in present time
+a=10;
+const PI=3.14; //constant
+console.log(a);
+console.log(PI);
+console.log(as);
+
+
+//data types
+let b=10;
+let fullName="tony stark";
+
+
