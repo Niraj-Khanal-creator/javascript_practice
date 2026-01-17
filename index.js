53077752cf612cf437bb6f4955990c7976f61974
nirajkhanal-hub <niraj.242019@ncit.edu.np>
2026-01-17 14:04:51 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function mysum()
+{
+let a=Number(document.getElementById("num1").value);
+let b=Number(document.getElementById("num1").value);
+let sum=a+b;
+document.getElementById("sum").innerHTML=+sum;
+}
